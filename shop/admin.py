199be155cc91d1de019d02b1ba9c1deee6e495c2
folegf76 <folegf76@gmail.com>
2023-02-20 21:07:29 +0200

    add admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Category, Product, Subcategory
+from .models import Category, Product, Subcategory, Brand, ProductsPhoto
 # Register your models here.
 
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Subcategory)
+admin.site.register(Brand)
+admin.site.register(ProductsPhoto)
