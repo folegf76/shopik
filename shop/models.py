791d9dffcdeeb614214de0f020785b64d3b3c443
folegf76 <folegf76@gmail.com>
2023-02-20 21:04:26 +0200

    change models

--- shop/models.py
+++ shop/models.py
@@ -8,6 +8,12 @@ class Category(models.Model):
     position = models.PositiveSmallIntegerField(unique=True)
     is_visible = models.BooleanField(default=True)
 
+    def __str__(self):
+        return f'{self.name}: {self.position}'
+
+    class Meta:
+        ordering = ('position', )
+
 
 class Subcategory(models.Model):
     name = models.CharField(max_length=50, unique=True)
@@ -15,18 +21,40 @@ class Subcategory(models.Model):
     is_visible = models.BooleanField(default=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'{self.name}: {self.position}'
+
+    class Meta:
+        ordering = ('position', )
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    position = models.PositiveSmallIntegerField(unique=True)
+    description = models.TextField(max_length=200, blank=True)
+    is_visible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.name}: {self.position}'
+
+    class Meta:
+        ordering = ('position', )
+
 
 class Product(models.Model):
     name = models.CharField(max_length=100, db_index=True, unique=True)
     slug = models.SlugField(max_length=100, db_index=True)
+    code_self = models.CharField(max_length=20, unique=True, blank=True)
     position = models.PositiveSmallIntegerField()
     subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE)
+    in_brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     description = models.TextField(max_length=200, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     is_visible = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    on_sale = models.BooleanField(default=False)
 
     class Meta:
         ordering = ('name', )
@@ -37,4 +65,9 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse("shop:product_detail", args=[self.id, self.slug])
+
+
+class ProductsPhoto(models.Model):
+    id_product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
 
