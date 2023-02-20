3f221326444452eb3972aa57af9f35793ae3f8cd
folegf76 <folegf76@gmail.com>
2023-02-20 20:04:12 +0200

    correct models

--- shop/models.py
+++ shop/models.py
@@ -1,16 +1,30 @@
 from django.db import models
 from django.urls import reverse
-
 # Create your models here.
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    position = models.PositiveSmallIntegerField(unique=True)
+    is_visible = models.BooleanField(default=True)
+
+
+class Subcategory(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    position = models.PositiveSmallIntegerField(unique=True)
+    is_visible = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
 class Product(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, unique=True)
     slug = models.SlugField(max_length=100, db_index=True)
+    position = models.PositiveSmallIntegerField()
+    subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
-    description = models.TextField(blank=True)
+    description = models.TextField(max_length=200, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    available = models.BooleanField(default=True)
+    is_visible = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
