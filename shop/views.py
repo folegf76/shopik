175bfa923f4d327c8dc620abf57569dbb2fb96c3
folegf76 <folegf76@gmail.com>
2023-02-24 15:23:36 +0200

    breakdown index

--- shop/views.py
+++ shop/views.py
@@ -6,7 +6,8 @@ from .models import Product, Category, Subcategory, Brand
 
 def shop(request):
     categories = Category.objects.filter(is_visible=True)
-    return render(request, 'index.html')
+    sale_all = Product.objects.filter(on_sale=True)
+    return render(request, 'shop.html', context={'sale_all': sale_all})
 
 
 # def product_list(request, category_slug=None):
