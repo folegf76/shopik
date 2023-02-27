b0303743fc5a52dbc3374ea810c6561348826a87
folegf76 <folegf76@gmail.com>
2023-02-27 19:30:49 +0200

    add login, logout

--- account/forms.py
+++ account/forms.py
@@ -1,7 +1,23 @@
 from django import forms
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 
 User = get_user_model()
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'type': "text"}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'type': "password"}))
+
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if not user or not user.check_password(password):
+                raise forms.ValidationError('Логін або пароль не вірні')
+        return super().clean()
+
 
 class RegistrationForm(forms.ModelForm):
     class Meta:
@@ -10,7 +26,7 @@ class RegistrationForm(forms.ModelForm):
 
     username = forms.CharField(widget=forms.TextInput(attrs={'type': "text"}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'type': "password"}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'type': "password2"}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'type': "password"}))
 
     def clean_password2(self):
         if self.cleaned_data['password'] == self.cleaned_data['password2']:
