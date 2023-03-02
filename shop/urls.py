10e0410e13e96b1509228ee43d08d56e76d542a0
folegf76 <folegf76@gmail.com>
2023-03-02 18:30:30 +0200

    add cart

--- shop/urls.py
+++ shop/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'shop'
 
 urlpatterns = [
-    path('', views.shop),
-    # path('', views.product_list, name='product_list'),
-    # path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('', views.shop, name='shop_list'),
+    path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,29 +1,25 @@
 from django.shortcuts import get_object_or_404, render
-
-# from cart.cart import Cart
+from cart.cart import Cart
 from .models import Product, Category, Subcategory, Brand
 
 
 def shop(request):
+    cart = Cart(request)
     categories = Category.objects.filter(is_visible=True)
     sale_all = Product.objects.filter(on_sale=True)
     products = Product.objects.filter(is_visible=True)
     return render(request, 'shop.html', context={
         'sale_all': sale_all,
         'products': products,
+        'cart': cart,
+        'categories': categories,
     })
 
 
-# def product_list(request, category_slug=None):
-#     cart = Cart(request)
-#     products = Product.objects.filter(available=True)
-#     return render(request, 'list.html', {'products': products, 'cart': cart})
-#
-#
-# def product_detail(request, id, slug):
-#     product = get_object_or_404(Product, id=id, slug=slug, available=True)
-#     return render(request,
-#                   'product_detail.html',
-#                   {'product': product}
-#                   )
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, is_visible=True)
+    return render(request,
+                  'product-details.html',
+                  {'product': product}
+                  )
 
