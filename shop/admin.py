c1ffb6a0667f1b628bee07fb3fa048ce517e4240
folegf76 <folegf76@gmail.com>
2023-03-05 11:57:28 +0200

    set up the admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -2,11 +2,6 @@ from django.contrib import admin
 from .models import Category, Product, Subcategory, Brand, ProductsPhoto
 # Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Subcategory)
-admin.site.register(Brand)
-admin.site.register(ProductsPhoto)
-
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
@@ -14,3 +9,31 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ['is_visible', 'created', 'updated']
     list_editable = ['price', 'is_visible']
     prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ['name', 'is_visible', 'position', 'description']
+    list_filter = ['is_visible', 'position']
+    list_editable = ['is_visible', 'position']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'is_visible', 'position']
+    list_filter = ['is_visible', 'position']
+    list_editable = ['is_visible', 'position']
+
+
+@admin.register(Subcategory)
+class SubcategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'is_visible', 'position', 'category']
+    list_filter = ['is_visible', 'position', 'category']
+    list_editable = ['is_visible', 'position', 'category']
+
+
+@admin.register(ProductsPhoto)
+class ProductsPhotoAdmin(admin.ModelAdmin):
+    list_display = ['id_product', 'image']
+    list_filter = ['id_product']
+    list_editable = ['image']
